1ee597591c6461c910f003eccb960fa2f45f0d0e
Sabri <sabri.sonmez@macaulay.cuny.edu>
2019-02-08 15:20:25 -0500

    Initial Commit

---
+++ SmackApp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmackApp2
+//
+//  Created by Sabri Sönmez on 2/8/19.
+//  Copyright © 2019 Sabri Sönmez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
